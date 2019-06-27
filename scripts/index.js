90df0a5dc62a4a1191719dc6ef9d88f33db6ce61
nolanjharris <thenolanjames@gmail.com>
2019-06-27 14:48:06 -0500

    completed project

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,13 @@
-console.log('it works')
+const form = document.querySelector('#chat-form');
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const input = document.querySelector('#chat-input');
+    const chatContent = document.querySelector("#chat-content");
+    // chatContent.innerHTML('<p><strong>You: </strong></p>' + input.value);
+    const newChat = document.createElement('p');
+    newChat.innerText = input.value;
+    chatContent.append(newChat);
+
+    input.value = '';
+})
